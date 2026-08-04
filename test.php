a0511e51668855fa4e130c8ce267fe4d44707e57
linducip2208 <lindu@act.co.id>
2026-08-04 11:41:36 +0700

    chore: remove test file

--- test.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-require __DIR__.'/vendor/autoload.php';
-\ = require_once __DIR__.'/bootstrap/app.php';
-\->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-\ = App\Models\User::where('email','admin@bengkel.test')->first();
-echo \->email . ' | password check: ' . (Hash::check('password', \->password) ? 'OK' : 'FAIL');
