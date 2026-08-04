b3a7679491d0a78b3b569c34a6c3c5e1ebc9d7b0
linducip2208 <lindu@act.co.id>
2026-08-04 15:06:57 +0700

    fix: set global pagination defaultView to partials.pagination (chevron icons)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Paginator::defaultView('partials.pagination');
+
         \Illuminate\Support\Facades\Blade::directive('money', function ($expression) {
             return "<?php echo \\App\\Models\\Currency::format($expression); ?>";
         });
